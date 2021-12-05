d0014330d9f8ebb49cbb58c1cc34a1bfc8e6ef64
Michael Bayucot <mbayucot@gmail.com>
2021-12-05 09:33:08 +0800

    Add article db

--- db/schema.rb
+++ db/schema.rb
@@ -11,4 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_12_05_011156) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
